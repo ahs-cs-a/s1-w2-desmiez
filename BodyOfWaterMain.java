094258851b66278bc594bfb61f897d2f1835a932
desmiez <desmiez26@ausdk12.org>
2024-08-23 17:02:01 -0700

    BodyOfWaterMain.java

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -2,10 +2,12 @@ public class BodyOfWaterMain {
     public static void main(String[] args) {
         BodyOfWater b = new BodyOfWater("Lake M", 500, 30.5, false);
         BodyOfWater b1 = new BodyOfWater("Lake N", 200, 15.5, true);
-
+        
+        //Print out each attribute using the getter on each instance//
         System.out.println(b.getName());
         System.out.println(b1.getName());
-
+        //On one instance call the setName method//
+        
 
     }
 
