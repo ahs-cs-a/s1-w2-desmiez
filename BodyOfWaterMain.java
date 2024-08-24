36823b1088aa58fa6371f5241de4f955918f0773
desmiez <desmiez26@ausdk12.org>
2024-08-23 18:18:03 -0700

    BodyOfWaterMain.java

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -6,9 +6,10 @@ public class BodyOfWaterMain {
         //Print out each attribute using the getter on each instance//
         System.out.println(b.getName());
         System.out.println(b1.getName());
-        //On one instance call the setName method//
         
-
+        //On one instance call the setName method//
+        b.setname("Lake K");
+        System.out.println(b.getname());
     }
 
 }
